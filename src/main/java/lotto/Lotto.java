4b62982227de403ee59c12f79d76a8f3d237a14b
junhokim <kokm333@gmail.com>
2024-03-13 17:27:57 +0900

    sortNumbers(): 뽑은 로또번호 정렬해준느 함수 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,14 +1,19 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    Application application = new Application();
+
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
+
+
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
@@ -16,5 +21,9 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+
+    public void sortNumbers(){
+        Collections.sort(this.numbers);
+    }
+
 }
