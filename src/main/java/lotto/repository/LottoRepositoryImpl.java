6f4ee5e53dfbacd35d82ca3f488f651f68a66838
GunHa Hwang <hgh1472@naver.com>
2024-03-14 14:38:02 +0900

    refactor: implements 코드 추가

--- src/main/java/lotto/repository/LottoRepositoryImpl.java
+++ src/main/java/lotto/repository/LottoRepositoryImpl.java
@@ -5,7 +5,7 @@ import lotto.domain.Lotto;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LottoRepositoryImpl {
+public class LottoRepositoryImpl implements LottoRepository {
     private List<Lotto> lottos = new ArrayList<>();
 
     public void save(Lotto lotto) {
