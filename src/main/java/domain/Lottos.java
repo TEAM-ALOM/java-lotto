02f031b7ade793215670dc8a7ded7f237cceafae
Kimseungin0529 <whffkaos007@naver.com>
2024-03-15 00:17:27 +0900

    feat(LottoWinnerNumber) : 로또 당첨 번호 클래스 구현

--- src/main/java/domain/Lottos.java
+++ src/main/java/domain/Lottos.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Lottos {
-    private List<Lotto> lottos= new ArrayList<>();
+    private List<Lotto> lottos =  new ArrayList<>();
 
     public Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
