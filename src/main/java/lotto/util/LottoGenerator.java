a3a61c8b4b3caf482f9e2c011bf1faaaf2309f31
GunHa Hwang <hgh1472@naver.com>
2024-03-14 22:47:20 +0900

    refactor: generateLotto 메서드 static 메서드로 수정

--- src/main/java/lotto/util/LottoGenerator.java
+++ src/main/java/lotto/util/LottoGenerator.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.kokodak.Randoms;
 public class LottoGenerator {
 
-    public List<Integer> generateLotto() {
+    static public List<Integer> generateLotto() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 }
