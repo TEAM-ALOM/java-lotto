d095e13ceeee7d6a8b28b7273b850bf996c8caa9
남종혁 <namjjong99@namjonghyeog-ui-MacBookPro.local>
2024-03-15 03:53:46 +0900

    first

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+
+        Scanner s = new Scanner(System.in);
+
+        int PriceAmount = s.nextInt();
+
+        PriceAmount = PriceAmount / 1000;
+
+        System.out.println(PriceAmount + "개를 구매했습니다.");
+
     }
 }
