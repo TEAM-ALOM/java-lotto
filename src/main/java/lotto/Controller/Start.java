eac8b5713eb57ccbc20a74be9a11a194de46febc
ryansewook <ryansewook@naver.com>
2024-03-16 00:46:35 +0900

    Commit

--- src/main/java/lotto/Controller/Start.java
+++ src/main/java/lotto/Controller/Start.java
@@ -27,9 +27,7 @@ public class Start {
         myRandomLottoList = setRandomLottoList(LottoAmount); //LottoAmount 개의 랜덤 로또 번호 생성
 
         System.out.println(LottoAmount+"개를 구매하셨습니다");
-
-        for(List<Integer> LottoList : myRandomLottoList){ //랜덤으로 생성된 로또 출력
-            System.out.println(LottoList);  }
+        inOut.OutLottoList(myRandomLottoList); //랜덤으로 생성된 로또 출력
 
 
         List<Integer> WinningLotto = inOut.InWinningLotto(); //로또 당첨 번호 입력
@@ -39,7 +37,9 @@ public class Start {
             int Count = getResult(LottoList,WinningLotto);
             setResult(Count,WinningLotto,BonusNum); }
 
-        ResultStatistics(LottoAmount); //결과 출력
+        double RateOfReturn = getRateOfReturn(getResultMoney(),LottoAmount*1000); //수익률
+
+        inOut.OutResultStatistics(FIRST,SECOND,THIRD,FORTH,FIVTH,RateOfReturn); //결과 출력
 
 
 
@@ -96,16 +96,10 @@ public class Start {
         return Math.round(a*10.0)/10.0;
     }
 
-
-    private void ResultStatistics(int LottoAmount){ //로또 최종 통계 출력
-        int ResultMoney = FIRST*2000000000 + SECOND*30000000 + THIRD*1500000 + FORTH*50000 + FIVTH*5000;
-        System.out.println("당첨 통계");
-        System.out.println("---");
-        System.out.println("3개 일치 (5,000원) - "+FIVTH+"개");
-        System.out.println("4개 일치 (50,000원) - "+FORTH+"개");
-        System.out.println("5개 일치 (1,500,000원) - "+THIRD+"개");
-        System.out.println("5개 일치, 보너스 볼 일치(30,000,000원) - "+SECOND+"개");
-        System.out.println("6개 일치 (2,000,000,000원) - "+FIRST+"개");
-        System.out.println("총 수익률은 "+getRateOfReturn(ResultMoney,LottoAmount*1000)+"% 입니다");
+    private double getResultMoney(){
+        return FIRST*2000000000 + SECOND*30000000 + THIRD*1500000 + FORTH*50000 + FIVTH*5000;
     }
+
+
+
 }
--- src/main/java/lotto/View/InOut.java
+++ src/main/java/lotto/View/InOut.java
@@ -29,4 +29,22 @@ public class InOut {
         return Integer.parseInt(Console.readLine());
     }
 
+    public void OutResultStatistics(int FIRST,int SECOND,int THIRD,int FORTH,int FIVTH,double RateOfReturn){ //로또 최종 통계 출력
+        int ResultMoney = FIRST*2000000000 + SECOND*30000000 + THIRD*1500000 + FORTH*50000 + FIVTH*5000;
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - "+FIVTH+"개");
+        System.out.println("4개 일치 (50,000원) - "+FORTH+"개");
+        System.out.println("5개 일치 (1,500,000원) - "+THIRD+"개");
+        System.out.println("5개 일치, 보너스 볼 일치(30,000,000원) - "+SECOND+"개");
+        System.out.println("6개 일치 (2,000,000,000원) - "+FIRST+"개");
+        System.out.println("총 수익률은 "+RateOfReturn+"% 입니다");
+    }
+
+    public void OutLottoList(List<List> MyRandomLottoList){ //랜덤으로 생성된 로또 출력
+        for(List<Integer> myList : MyRandomLottoList){
+            System.out.println(myList);
+        }
+    }
+
 }
