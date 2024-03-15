9bd12c2bfc792edd754c4ac875c52fe03d4af5e1
junhokim <kokm333@gmail.com>
2024-03-15 21:46:42 +0900

    로또번호 정렬

---
+++ src/main/java/lotto/SortLottoNumber.java
@@ -0,0 +1,11 @@
+package lotto;
+
+import java.util.Collections;
+import java.util.List;
+
+public class SortLottoNumber {
+    public static List<Integer> sortNumbers(List<Integer> numbers){
+        Collections.sort(numbers);
+        return numbers;
+    }
+}
