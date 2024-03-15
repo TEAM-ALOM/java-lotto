1678ca77dbca7c4ed26a0842c903a08a567b3520
Kimseungin0529 <whffkaos007@naver.com>
2024-03-15 18:23:49 +0900

    feat(LottoController) : lottoStart 메서드 구현

--- src/main/java/view/Input.java
+++ src/main/java/view/Input.java
@@ -14,23 +14,30 @@ public class Input {
 
     public static int inputPurchaseAmount() throws IOException {
         System.out.println("구입금액을 입력해 주세요.");
-
-        int purchaseAmount = Integer.parseInt(Console.readLine());
-
-        return purchaseAmount;
+        try {
+            return Integer.parseInt(Console.readLine());
+        }catch (IllegalArgumentException e){
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해야 합니다.");
+        }
     }
     public static List<Integer> inputWinnerNumbers(){
 
         System.out.println("당첨 번호를 입력해 주세요.");
-        String str = Console.readLine();
-        List<Integer> winnerNumbers = Arrays.stream(str.split(",")).map(ch -> Integer.parseInt(ch)).collect(Collectors.toList());
-        return winnerNumbers;
+        try {
+            String str = Console.readLine();
+            return Arrays.stream(str.split(",")).map(ch -> Integer.parseInt(ch)).collect(Collectors.toList());
+        }catch (IllegalArgumentException e){
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해야 합니다.");
+        }
     }
 
     public static int inputBonusNumber(){
         System.out.println("보너스 번호를 입력해 주세요.");
-        int bonusNumber = Integer.parseInt(Console.readLine());
-        return bonusNumber;
+        try {
+            return Integer.parseInt(Console.readLine());
+        }catch (IllegalArgumentException e){
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해야 합니다.");
+        }
     }
 
 
