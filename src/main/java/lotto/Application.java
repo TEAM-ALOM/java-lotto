2e6271a2f4911572bf62be86112573ebb83f6de7
rndnjsgud <00kwh@naver.com>
2024-03-16 03:37:29 +0900

    Duplicate Error Implementation

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Scanner;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int purchaseAmount;
+        System.out.println("구입 금액을 입력해 주세요");
+        purchaseAmount = Integer.parseInt(Console.readLine());
+        if(purchaseAmount < 1000){
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 이상이어야 합니다");
+        }
+
+
+
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,17 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        checkDuplicate(numbers);
         this.numbers = numbers;
     }
 
@@ -17,4 +22,36 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private List<Integer> generateNumbers(){
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1,45,6);    //겹치지 않는 숫자 반환
+
+        numbers.stream()
+                .sorted();
+        return numbers;
+    }
+
+    private boolean checkBonusNumber(List<Integer> numbers, int bonusNumber){       //보너스 숫자 일치 여부 확인
+        if(numbers.contains(bonusNumber)){
+            return true;
+        }
+        return false;
+    }
+
+    private int checkWinningNumbers(List<Integer> numbers, List<Integer> winningNumbers){   //일치하는 숫자 개수 확인
+        int cnt = 0;
+        for(int i = 0 ; i < numbers.size() ; i++){
+            if(winningNumbers.contains(numbers.get(i))){
+                cnt += 1;
+            }
+        }
+        return cnt;
+    }
+
+    private void checkDuplicate(List<Integer> numbers){    //중복 숫자 확인
+        Set<Integer> set =  new HashSet<>(numbers);
+
+        if(numbers.size() != set.size()){
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자가 존재합니다.");
+        }
+    }
 }
