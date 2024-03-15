f9cb740720ad90e2e5cdf60f6993b4c19a9e601e
junhokim <kokm333@gmail.com>
2024-03-15 22:36:33 +0900

    구매한 개수만큼 로또객체 생성하고 당첨번호와 보너스넘버 입력까지

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,27 @@
 package lotto;
 
+import lotto.winningLotto.WinningLotto;
+import lotto.WinPriceNumber;
+
+import static lotto.WinPriceNumber.getLottoBonusNumber;
+import static lotto.WinPriceNumber.getWinPriceNumber;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        final int purchaseAmount = BuyLotto.getPurchaseAmount();
+        Lotto[] lotto = new Lotto[purchaseAmount];
+
+        System.out.println(purchaseAmount + "개 구매했습니다.");
+
+        for (int i = 0; i < purchaseAmount; i++) {
+            lotto[i] = new Lotto(CreateRandomLottoNumbers.createRandomLottoNumbers());
+            lotto[i].printNumbers();
+        }
+
+        WinningLotto winningLotto = new WinningLotto(getWinPriceNumber(), getLottoBonusNumber());
+
+
     }
 }
