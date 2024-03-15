c7e4fa5608a9a6e6a81b9f9c9240beab6b12e591
junhokim <kokm333@gmail.com>
2024-03-15 21:54:12 +0900

    set->get

--- src/main/java/lotto/WinPriceNumber.java
+++ src/main/java/lotto/WinPriceNumber.java
@@ -12,7 +12,7 @@ public class WinPriceNumber {
     private static final String LOTTO_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String LOTTO_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
-    public static List<Integer> setWinPriceNumber() {
+    public static List<Integer> getWinPriceNumber() {
         List<Integer> numbers = new ArrayList<>();
         System.out.println(LOTTO_NUMBER_MESSAGE);
         String[] str = readLine().split(",");
