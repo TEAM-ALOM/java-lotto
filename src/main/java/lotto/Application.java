1d0c8eb5c1980ea11115616fa539efe1b18dfed7
junhokim <kokm333@gmail.com>
2024-03-16 14:25:19 +0900

    메세지 상수화

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,12 +6,14 @@ import lotto.winningLotto.InputWinPriceNumber;
 
 public class Application {
 
+    private static final String HOW_MANY_LOTTO_USER_PURCHASED_MESSAGE = "%d개를 구매했습니다.\n";
+
     public static void main(String[] args) {
 
         final int purchaseAmount =  BuyLotto.getPurchaseAmount();
         Lotto[] lotto = new Lotto[purchaseAmount];
 
-        System.out.println(purchaseAmount + "개 구매했습니다.");
+        System.out.printf(HOW_MANY_LOTTO_USER_PURCHASED_MESSAGE,purchaseAmount);
 
         for (int i = 0; i < purchaseAmount; i++) {
             lotto[i] = new Lotto(CreateRandomLottoNumbers.createRandomLottoNumbers());
