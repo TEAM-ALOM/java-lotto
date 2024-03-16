9c71d08e2f9e466828b2b37c435f6c89bad3b3b7
GunHa Hwang <hgh1472@naver.com>
2024-03-16 22:50:14 +0900

    feat: LottoResultDto에 수익율 필드 추가에 따른 추가 메서드 구현

--- src/main/java/lotto/domain/LottoResultDto.java
+++ src/main/java/lotto/domain/LottoResultDto.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public class LottoResultDto {
     // 등수, 당첨 로또의 개수
     private Map<Integer, Integer> result = new HashMap<>();
+    private Double rate;
 
     public LottoResultDto() {
         result.put(1, 0);
@@ -15,11 +16,38 @@ public class LottoResultDto {
         result.put(5, 0);
     }
 
+    public void setRate(Double rate) {
+        this.rate = rate;
+    }
+
     public Map<Integer, Integer> getResult() {
         return result;
+    }
+
+    public Double getRate() {
+        return rate;
     }
 
     public void setResult(Map<Integer, Integer> result) {
         this.result = result;
     }
+
+    public Integer getFirstCount() {
+        return result.get(1);
+    }
+
+    public Integer getSecondCount() {
+        return result.get(2);
+    }
+
+    public Integer getThirdCount() {
+        return result.get(3);
+    }
+    public Integer getFourthCount() {
+        return result.get(4);
+    }
+    public Integer getFifthCount() {
+        return result.get(5);
+    }
+
 }
