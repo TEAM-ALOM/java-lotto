cf7adb50ce5cabb3d30f0826c92201e9acb9128d
o0Orangee <maboongee@gmail.com>
2024-03-16 22:36:26 +0900

    로또 일치 확인

--- src/main/java/lotto/domain/WinningDetails.java
+++ src/main/java/lotto/domain/WinningDetails.java
@@ -1,5 +1,23 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class WinningDetails {
+
+//    로또 1개와 당첨번호 비교, 일치하는 개수 찾기(스트림 필터)
+    private static int compareWithWinningNums(Lotto lotto, LottoWinningNumber lottoWinningNumber) {
+        List<Integer> nums = lotto.getNumbers();
+        List<Integer> winningNums = lottoWinningNumber.getWinningNums();
+        return (int) nums.stream()
+                .filter(winningNums::contains)
+                .count();
+    }
+
+//    5개 일치 시에만 호출하면 되지 않나?
+    private static boolean compareWithBonusNum(Lotto lotto, LottoWinningNumber lottoWinningNumber/*, int matchCount*/) {
+        List<Integer> nums = lotto.getNumbers();
+        int bonusNum = lottoWinningNumber.getBonusNum();
+        return nums.contains(bonusNum);     //있으면 true 없으면 false return
+    }
 
 }
