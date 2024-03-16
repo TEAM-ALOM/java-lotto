7bc675473213c3229cd607f14ca224890cc4f907
SongHyeonJun <hoo5152000@naver.com>
2024-03-16 14:41:52 +0900

    로또 구매, 번호 출력 구현

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,4 +1,24 @@
 package controller;
 
+import lotto.GenerateLotto;
+import lotto.Lottos;
+import view.InputView;
+import view.OutputView;
+
 public class LottoController {
+    public void startLotto() {
+        try {
+            int cost = InputView.buyLotto();
+            GenerateLotto generateLotto = new GenerateLotto(cost);
+            Lottos lottos = new Lottos(generateLotto.generateLottos());
+
+            OutputView.printHowManyLottoUserPurchased(generateLotto.getCount());
+            OutputView.printLottos(lottos);
+
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
