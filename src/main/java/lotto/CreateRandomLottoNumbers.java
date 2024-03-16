4d524a8c49d810fc56e9a0a3f911291656bef7f8
junhokim <kokm333@gmail.com>
2024-03-16 14:21:11 +0900

    불필요한 import 제거

--- src/main/java/lotto/CreateRandomLottoNumbers.java
+++ src/main/java/lotto/CreateRandomLottoNumbers.java
@@ -4,10 +4,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
 
-import static lotto.SortLottoNumber.sortNumbers;
-
 public class CreateRandomLottoNumbers {
     public static List<Integer> createRandomLottoNumbers() {
-        return sortNumbers(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        return SortLottoNumber.sortNumbers(Randoms.pickUniqueNumbersInRange(1, 45, 6));
     }
 }
