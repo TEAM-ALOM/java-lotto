192bb5a4e9f2361bfddc0be68f3ae4f326d4200e
SongHyeonJun <hoo5152000@naver.com>
2024-03-16 14:46:49 +0900

    컨트롤러 실행 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,15 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import controller.LottoController;
 
 import static java.lang.Integer.*;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController();
+        lottoController.startLotto();
     }
 
 }
