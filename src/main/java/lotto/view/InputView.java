83c85b946cd56fa1fc0cb7227fd9e2523aee4930
GunHa Hwang <hgh1472@naver.com>
2024-03-16 22:44:16 +0900

    feat: 입력값을 받는 InputView 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,13 +1,44 @@
 package lotto.view;
 
+import lotto.constant.ViewConst;
 import lotto.util.ValidationFilter;
-import org.kokodak.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class InputView {
+    Scanner scanner = new Scanner(System.in);
+
+    public String readLine() {
+        return scanner.nextLine();
+    }
 
     public Integer readMoney() {
         System.out.println(ViewConst.MONEY_REQUEST.getMessage());
-        String money = Console.readLine();
-        ValidationFilter.isNumber(money);
-        return Integer.parseInt(money);
+        String money = readLine();
+        return ValidationFilter.isNumber(money);
+    }
+
+    public List<Integer> getWinningNumbers() {
+        System.out.println(ViewConst.WINNING_NUMBER_REQUEST.getMessage());
+        String numbers = readLine();
+        String[] split = numbers.split(",");
+        List<Integer> winningNumbers = new ArrayList<>();
+        for (String s : split) {
+            Integer num = ValidationFilter.isNumber(s);
+            ValidationFilter.isInRange(num);
+            winningNumbers.add(num);
+        }
+        ValidationFilter.isSixNumbers(winningNumbers);
+        return winningNumbers;
+    }
+
+    public Integer getBonusNumber() {
+        System.out.println(ViewConst.BONUS_NUMBER_REQUEST.getMessage());
+        String number = readLine();
+        Integer bonusNumber = ValidationFilter.isNumber(number);
+        ValidationFilter.isInRange(bonusNumber);
+        return bonusNumber;
     }
 }
