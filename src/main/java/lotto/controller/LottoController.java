05a54cf87bfac7f0a5478bb5da6f421427d87afb
junhokim <kokm333@gmail.com>
2024-03-16 15:33:51 +0900

    컨트롤러의 구매량 출력 책임을 InputPurchaseAmount로 위임

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,25 +1,38 @@
 package lotto.controller;
 
-import lotto.domain.CreateRandomLottoNumbers;
-import lotto.domain.InputPurchaseAmount;
-import lotto.domain.Lotto;
-import lotto.domain.InputWinPriceNumber;
-import lotto.domain.WinningLotto;
+import lotto.domain.*;
+import lotto.view.OutputView;
+
+import java.util.Arrays;
+import java.util.Map;
 
 public class LottoController {
-    private static final String HOW_MANY_LOTTO_USER_PURCHASED_MESSAGE = "%d개를 구매했습니다.\n";
+    private static final int LOTTO_PRICE = 1000;
+
     public void lottoStart(){
+
         final int purchaseAmount =  InputPurchaseAmount.getPurchaseAmount();
         Lotto[] lotto = new Lotto[purchaseAmount];
-
-        System.out.printf(HOW_MANY_LOTTO_USER_PURCHASED_MESSAGE,purchaseAmount);
-
         for (int i = 0; i < purchaseAmount; i++) {
             lotto[i] = new Lotto(CreateRandomLottoNumbers.createRandomLottoNumbers());
-            lotto[i].printNumbers();
         }
+        Lottos lottos = new Lottos(Arrays.stream(lotto).toList());
+        printLottoInformation(purchaseAmount,lottos);
 
         WinningLotto winningLotto = new WinningLotto(InputWinPriceNumber.getWinPriceNumber(), InputWinPriceNumber.getLottoBonusNumber());
+        Map<WinningRank, Integer> winningDetails = WinningStatistics.getWinningDetails(lottos, winningLotto);
+        printWinningInformation(winningDetails, purchaseAmount);
+    }
 
+    private void printLottoInformation(int purchaseAmount, Lottos lottos){
+        InputPurchaseAmount.printPurchaseAmount(purchaseAmount);
+        Lottos.printLottos(lottos);
+    }
+
+    private void printWinningInformation(Map<WinningRank, Integer> winningDetails, int purchaseAmount) {
+        OutputView.printWinningStatistics();
+        OutputView.printWinningDetails(winningDetails);
+        long winningAmount = WinningStatistics.getWinningAmount(winningDetails);
+        OutputView.printLottoYield(WinningStatistics.getLottoYield(winningAmount, purchaseAmount*LOTTO_PRICE));
     }
 }
--- src/main/java/lotto/domain/InputPurchaseAmount.java
+++ src/main/java/lotto/domain/InputPurchaseAmount.java
@@ -3,10 +3,15 @@ package lotto.domain;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputPurchaseAmount {
+    private static final String HOW_MANY_LOTTO_USER_PURCHASED_MESSAGE = "%d개를 구매했습니다.\n";
     private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     public static int getPurchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_MESSAGE);
         String str = Console.readLine();
         return Integer.parseInt(str) / 1000;
+    }
+
+    public static void printPurchaseAmount(int purchaseAmount){
+        System.out.printf(HOW_MANY_LOTTO_USER_PURCHASED_MESSAGE, purchaseAmount);
     }
 }
