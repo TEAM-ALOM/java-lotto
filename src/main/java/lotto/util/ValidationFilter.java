72a8f7d6c42454126e374f8db041830db72a1360
GunHa Hwang <hgh1472@naver.com>
2024-03-16 22:45:31 +0900

    feat: 로또 숫자의 개수, 숫자의 범위 검증 기능 구현

--- src/main/java/lotto/util/ValidationFilter.java
+++ src/main/java/lotto/util/ValidationFilter.java
@@ -1,6 +1,8 @@
 package lotto.util;
 
-import lotto.exception.ExceptionConst;
+import lotto.constant.ExceptionConst;
+
+import java.util.List;
 
 public class ValidationFilter {
 
@@ -12,5 +14,18 @@ public class ValidationFilter {
             throw new IllegalArgumentException(ExceptionConst.NOT_INTEGER_INPUT_EXCEPTION.getMessage());
         }
         return m;
+    }
+
+    static public List<Integer> isSixNumbers(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(ExceptionConst.LOTTO_COUNT_EXCEPTION.getMessage());
+        }
+        return numbers;
+    }
+
+    static public void isInRange(Integer number) {
+        if (number > 45 || number < 1) {
+            throw new IllegalArgumentException(ExceptionConst.NUMBER_RANGE_EXCEPTION.getMessage());
+        }
     }
 }
