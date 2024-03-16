d50df9b81bf1ff7f246705ef395abdf6aa9d0e4b
강지윤 <23011595@sju.ac.kr>
2024-03-16 12:06:52 +0900

    오류가 발생했을 때 문구 출력 후 코드를 종료시키는 클래스

--- src/main/java/lotto/UserHelp.java
+++ src/main/java/lotto/UserHelp.java
@@ -2,8 +2,24 @@ package lotto;
 
 public class UserHelp {
 
-    public static void error() {
-        System.out.println("[ERROR]");
+    public static void errorUserBuy() {
+        System.out.println("[ERROR] 지불한 금액이 많거나 적습니다.");
+        throw new IllegalArgumentException();
+    }
+
+    public static void errorLuckyNumber_size() {
+        System.out.println("[ERROR] 로또 번호의 개수는 6개입니다.");
+        throw new IllegalArgumentException();
+    }
+
+    public static void errorLuckyNumber_number() {
+        System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        throw new IllegalArgumentException();
+    }
+
+    public static void errorPlusNumber() {
+        System.out.println("[ERROR] 보너스 번호는 로또 당첨 번호에 없는 숫자여야 합니다.");
+        throw new IllegalArgumentException();
     }
 
 }
