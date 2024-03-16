b251404b8c56d8bff8a1e70c3aa1be0fa27d73f4
junhokim <kokm333@gmail.com>
2024-03-16 14:20:47 +0900

    에러메세지 상수화

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
-
+    private static final String LOTTO_NUMBERS_MUST_BE_SIX_DIFFERENT_NUMBERS = "[ERROR] 로 번호는 서로 다른 6개의 수여야 합니다.";
+    private static final String LOTTO_NUMBERS_MUST_BE_SIX_UNIQUE_NUMBERS = "[ERROR] 로또 번호는 중복된 수가 있으면 안됩니다.";
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -14,14 +15,14 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            System.out.println("[ERROR] 로또 번호는 6개여야 합니다.");
+            System.out.println(LOTTO_NUMBERS_MUST_BE_SIX_DIFFERENT_NUMBERS);
             throw new IllegalArgumentException();
         }
     }
 
     private void validateDuplicate(List<Integer> numbers){
         if(numbers.size() != numbers.stream().distinct().count()){
-            System.out.println("[ERROR] 로또 번호는 중복된 수가 있으면 안됩니다.");
+            System.out.println(LOTTO_NUMBERS_MUST_BE_SIX_UNIQUE_NUMBERS);
             throw new IllegalArgumentException();
         }
     }
