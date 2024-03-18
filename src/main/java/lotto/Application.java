63ea69f8312ec27348e8130d57dd2fb6a2323fb0
20011730 <lsh76080m@naver.com>
2024-03-18 18:10:31 +0900

    로또 구입 금액 입력 (단위는 1,000원 단위)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        int lotto_price = 1000;
+
+        Scanner scanner = new Scanner(System.in);
+        int money = scanner.nextInt();
+
     }
 }
