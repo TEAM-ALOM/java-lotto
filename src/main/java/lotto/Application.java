4ba2f49c51ee2c09d11586ade26d6f00d596086f
swissmissed <shkim7020@naver.com>
2024-03-18 18:23:48 +0900

    입력 미완성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,25 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        Scanner scanner = new Scanner(System.in);
+        int purchase_num = scanner.nextInt();   //로또 구입 금액
+        List<Integer> correctNumber = new ArrayList<>();
+
+        String s = scanner.next();
+        String[] n = s.split(",");
+        for(int i=0;i<n.length;i++)
+            correctNumber.add(Integer.parseInt(n[i]));
+
+
+
     }
 }
