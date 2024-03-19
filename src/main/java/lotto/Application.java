030100688da6fa12eeea3574be224576c55b0303
nahyunKoo <angelcindy09@gmail.com>
2024-03-20 01:27:02 +0900

    로또 당첨번호랑 비교하는 메소드 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,13 @@
 package lotto;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
 
         int purchaseAmount, numLotto;
         List<Lotto> purchasedLottos = new ArrayList<>();
+
+        int[] result = {0, 0, 0, 0, 0, 0};
 
         purchaseAmount = InputHelper.getIntInput("구입금액을 입력해 주세요.");
 
@@ -28,15 +28,20 @@ public class Application {
             //보너스 번호 입력받기
             throw new IllegalArgumentException("[ERROR]번호의 범위는 1~45입니다.");
         }
+        Collections.sort(winningNumbers);
 
-        int n = InputHelper.getIntInput("보너스 번호를 입력해 주세요.");
+        //보너스 번호 입력받기
+        int bonus = InputHelper.getIntInput("보너스 번호를 입력해 주세요.");
         //보너스 번호 범위 확인
-        if(n < 1 || n > 45){
+        if(bonus < 1 || bonus > 45){
             throw new IllegalArgumentException("[ERROR]번호의 범위는 1~45입니다.");
         }
 
-        winningNumbers.add(n);
 
-
+        for(int i = 0; i<purchasedLottos.size(); i++){
+           int rank = Lotto.checkWinningResults(winningNumbers, purchasedLottos.get(i), bonus);
+           result[rank] += 1;
+        }
+        
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -51,30 +51,20 @@ public class Lotto {
         return true;
     }
 
-    public static void printWinningResults(List<Integer> winningNums, List<Lotto> lottos, int purchaseP) {
-        int first, second, third, fourth, fifth;
-        double earningRate;
-        System.out.println("당첨 통계");
-        System.out.println("---");
-        System.out.println("3개 일치 (5,000원) - " + fifth + "개");
-        System.out.println("4개 일치 (50,000원) - " + fourth + "개");
-        System.out.println("5개 일치 (1,500,000원) - " + third + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + second + "개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + first + "개");
-        earningRate = (fifth * 5 + fourth * 50 + third * 1500 + second * 30000 + first * 2000000) / (purchaseP / 1000) * 100;
-        System.out.println("총 수익률은 " + earningRate + "입니다.");
 
-    }
-
-    public static int checkWinningResults(List<Integer> winningNums, List<Lotto> lottos, int num){
+    public static int checkWinningResults(List<Integer> winningNums, Lotto lottos, int bonusN){
         int numOfWinnings = 0;
-        for(int i = 0; i<lottos.size(); i++){
-            for(int j = 0; j < 6; i++){
-
-            }
+        for (int number : lottos.numbers) {
+             if (winningNums.contains(number)) numOfWinnings++;
+         }
+        if(numOfWinnings == 0 || numOfWinnings == 1 || numOfWinnings == 2) return 0;
+        if(numOfWinnings == 3) return 5;
+        if(numOfWinnings == 4) return 4;
+        if(numOfWinnings == 5){
+            if (lottos.numbers.contains(bonusN))
+                return 2;
+            return 3;
         }
-        return numOfWinnings;
+        return 1;
     }
-
-    // TODO: 추가 기능 구현
 }
