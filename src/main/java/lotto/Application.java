2e7e081115eb0f2b6cbee13fb94f39cc1e4a9b13
nahyunKoo <angelcindy09@gmail.com>
2024-03-20 01:53:05 +0900

    메인메소드 간결하게 정리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,47 +3,61 @@ import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
-
-        int purchaseAmount, numLotto;
         List<Lotto> purchasedLottos = new ArrayList<>();
+        int[] result;
 
-        int[] result = {0, 0, 0, 0, 0, 0};
-
-        purchaseAmount = InputHelper.getIntInput("구입금액을 입력해 주세요.");
-
-        //구매한 로또 개수 구하고 출력
-        numLotto = Lotto.getNumberOfLotto(purchaseAmount);
+        //구입 금액 입력 받은 후 로또 개수 구하기
+        int purchaseAmount = InputHelper.getIntInput("구입금액을 입력해 주세요.");
+        int numLotto = Lotto.getNumberOfLotto(purchaseAmount);
         System.out.println(numLotto + "개를 구매했습니다.");
 
-        //로또 생성
+        //로또 생성 후 생성한 로또들 출력
         Lotto.generateLotto(purchasedLottos, numLotto);
-        //구매한 로또 출력
-        for(int i = 0; i<purchasedLottos.size(); i++){
-            System.out.println(purchasedLottos.get(i).getLottoNum());
-        }
+        Application.printPurchasedLottos(purchasedLottos);
 
         //당첨번호 입력받기
+        List<Integer> winningNumbers = getWinningNumbers();
+
+        //보너스 번호 입력받기
+        int bonus = getBonusNumber();
+
+        //로또 비교하기
+        result = getResult(purchasedLottos, winningNumbers, bonus);
+
+        //결과 출력하기
+        Lotto.printWinningResults(result, purchaseAmount);
+    }
+
+
+
+    private static void printPurchasedLottos(List<Lotto> purchased){
+        for(int i = 0; i<purchased.size(); i++){
+            System.out.println(purchased.get(i).getLottoNum());
+        }
+    }
+    private static List<Integer> getWinningNumbers() {
         List<Integer> winningNumbers = InputHelper.getListInput("당첨 번호를 입력해 주세요.");
-        if(Lotto.isWinningNumberValuable(winningNumbers) == false){
-            //보너스 번호 입력받기
+        if (!Lotto.isWinningNumberValuable(winningNumbers)) {
             throw new IllegalArgumentException("[ERROR]번호의 범위는 1~45입니다.");
         }
         Collections.sort(winningNumbers);
+        return winningNumbers;
+    }
 
-        //보너스 번호 입력받기
-        int bonus = InputHelper.getIntInput("보너스 번호를 입력해 주세요.");
-        //보너스 번호 범위 확인
-        if(bonus < 1 || bonus > 45){
+    private static int getBonusNumber(){
+        int bonusN = InputHelper.getIntInput("보너스 번호를 입력해 주세요.");
+        if(bonusN < 1 || bonusN > 45){
             throw new IllegalArgumentException("[ERROR]번호의 범위는 1~45입니다.");
         }
+        return bonusN;
+    }
 
-
-        for(int i = 0; i<purchasedLottos.size(); i++){
-           int rank = Lotto.checkWinningResults(winningNumbers, purchasedLottos.get(i), bonus);
-           result[rank] += 1;
+    private static int[] getResult(List<Lotto> purchased, List<Integer> winningNumbers, int bonus){
+        int[] result = {0, 0, 0, 0, 0, 0};
+        for(int i = 0; i<purchased.size(); i++){
+            int rank = Lotto.checkWinningResults(winningNumbers, purchased.get(i), bonus);
+            result[rank] += 1;
         }
-
-        Lotto.printWinningResults(result, purchaseAmount);
-
+        return result;
     }
 }
