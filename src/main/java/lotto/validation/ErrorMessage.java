cc4931e36fbd29693b0ccdde412f53c5125f6a7b
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2024-03-20 21:14:30 +0900

    Feat(ErrorMessage) : 당첨번호 입력 예외 문구 생성

--- src/main/java/lotto/validation/ErrorMessage.java
+++ src/main/java/lotto/validation/ErrorMessage.java
@@ -1,7 +1,9 @@
 package lotto.validation;
 
 public enum ErrorMessage {
-    MONEY_FOR_PURCHASE_NOT_NUMBER("구매금액은 숫자로 입력해주세요");
+    MONEY_FOR_PURCHASE_NOT_NUMBER("구매금액은 숫자로 입력해주세요"),
+    WINNINGNUMBERS_NOT_START_WITH_COMMA("쉼표로 시작할 수 없습니다."),
+    WINNINGNUMBERS_NOT_END_WITH_COMMA("쉼표로 끝날 수 없습니다.");
 
     private final String message;
 
