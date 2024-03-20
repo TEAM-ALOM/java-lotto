cd06328b5c34dab1f34238e0201f95bab2c91532
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2024-03-20 21:39:38 +0900

    Feat(ErrorMessage) : 보너스 번호 입력 에러 문구 추가

--- src/main/java/lotto/validation/ErrorMessage.java
+++ src/main/java/lotto/validation/ErrorMessage.java
@@ -3,7 +3,8 @@ package lotto.validation;
 public enum ErrorMessage {
     MONEY_FOR_PURCHASE_NOT_NUMBER("구매금액은 숫자로 입력해주세요"),
     WINNINGNUMBERS_NOT_START_WITH_COMMA("쉼표로 시작할 수 없습니다."),
-    WINNINGNUMBERS_NOT_END_WITH_COMMA("쉼표로 끝날 수 없습니다.");
+    WINNINGNUMBERS_NOT_END_WITH_COMMA("쉼표로 끝날 수 없습니다."),
+    WINNINGNUMBERS_NOT_DOUBLE_COMMA("쉼표 사이에 숫자를 넣어주세요");
 
     private final String message;
 
