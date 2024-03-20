1d43200d7cc1c79e340ec4b7d103b4e2fdb1aefd
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2024-03-20 21:13:53 +0900

    Feat(InputView) : 당첨번호 입력 기능

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,12 @@
 package lotto.view;
 
-import lotto.validation.ErrorMessage;
+import static lotto.validation.ErrorMessage.MONEY_FOR_PURCHASE_NOT_NUMBER;
+import static lotto.validation.ErrorMessage.WINNINGNUMBERS_NOT_END_WITH_COMMA;
+import static lotto.validation.ErrorMessage.WINNINGNUMBERS_NOT_START_WITH_COMMA;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.kokodak.Console;
 
 public class InputView {
@@ -18,7 +24,28 @@ public class InputView {
         try {
             Integer.parseInt(moneyForPurchase);
         } catch (NumberFormatException e) {
-            throw new IllegalStateException(ErrorMessage.MONEY_FOR_PURCHASE_NOT_NUMBER.getMessage());
+            throw new IllegalStateException(MONEY_FOR_PURCHASE_NOT_NUMBER.getMessage());
         }
+    }
+
+    private static void checkNotExistStartAndEndComma(String winningNumbers) {
+        if (winningNumbers.startsWith(",")) {
+            throw new IllegalStateException(WINNINGNUMBERS_NOT_START_WITH_COMMA.getMessage());
+        }
+        if (winningNumbers.endsWith(",")) {
+            throw new IllegalStateException(WINNINGNUMBERS_NOT_END_WITH_COMMA.getMessage());
+        }
+    }
+
+    public static List<Integer> winningNumbersInput() {
+        String winningNumbers = Console.readLine();
+        checkNotExistStartAndEndComma(winningNumbers);
+        return readNumbersFromConsole(winningNumbers);
+    }
+
+    public static List<Integer> readNumbersFromConsole(String winningNumbers) {
+
+        return Arrays.stream(winningNumbers.split(",")).map(InputView::changeInputToInt)
+                .collect(Collectors.toList());
     }
 }
