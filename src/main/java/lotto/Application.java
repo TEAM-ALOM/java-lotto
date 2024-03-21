d4991bcd179e144931b2b8b405f9abbf5c8dcb6f
20011730 <lsh76080m@naver.com>
2024-03-21 14:45:11 +0900

    로또 구입 금액 입력 - 단위는 1,000원 단위 - 1000원으로 나누어 떨어지지 않을시 예외처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -9,7 +9,16 @@ public class Application {
         int lotto_price = 1000;
 
         Scanner scanner = new Scanner(System.in);
+
+        //System.out.println("로또 구입 금액을 입력해주세요.");
+        System.out.println("Please enter the amount of lotto purchase.");
         int money = scanner.nextInt();
+        if (money % 1000 != 0){
+            throw new IllegalArgumentException("This is not an appropriate value. Please check the amount again.");
+        }
+
+        List<Integer> numbers = new ArrayList<>();
+
 
     }
 }
