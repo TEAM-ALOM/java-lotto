b7e760a941964978a66673a4ac79910053f0e003
20011730 <lsh76080m@naver.com>
2024-03-21 15:13:06 +0900

    로또 구입 금액 입력 - 단위는 1,000원 단위 - 1000원으로 나누어 떨어지지 않을시 예외처리
    당첨 번호 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,24 +1,45 @@
 package lotto;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO: 프로그램 구현
 
         int lotto_price = 1000;
 
         Scanner scanner = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         //System.out.println("로또 구입 금액을 입력해주세요.");
         System.out.println("Please enter the amount of lotto purchase.");
-        int money = scanner.nextInt();
+
+        // int money = scanner.nextInt();
+        int money = Integer.parseInt(br.readLine());
+
         if (money % 1000 != 0){
-            throw new IllegalArgumentException("This is not an appropriate value. Please check the amount again.");
+            throw new IllegalArgumentException("[ERROR] This is not an appropriate value. Please check the amount again.");
         }
 
         List<Integer> numbers = new ArrayList<>();
 
+        System.out.println("Please enter winning number.");
+
+        String str = br.readLine();
+        List<String> list = Arrays.asList(str.split(","));
+
+        if (list.size() > 6){
+            throw new IllegalArgumentException("[ERROR] There are too many winning numbers.");
+        }
+
+        for (String s : list){
+            numbers.add(Integer.parseInt(s));
+        }
+
+        System.out.println(numbers);
 
     }
 }
