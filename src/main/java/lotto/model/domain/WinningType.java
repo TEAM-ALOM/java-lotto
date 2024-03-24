e8aefb4ba6a697a515248566b58056d97bcef970
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2024-03-24 20:13:11 +0900

    Feat(WinningType) : 당첨종류 및 당첨금액

---
+++ src/main/java/lotto/model/domain/WinningType.java
@@ -0,0 +1,26 @@
+package lotto.model.domain;
+
+public enum WinningType {
+    MATCHES_SIX("6개 일치 (2,000,000,000원)", 2_000_000_000),
+    MATCHES_FIVE_WITH_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원)", 30_000_000),
+    MATCHES_FIVE("5개 일치 (1,500,000원)", 1_500_000),
+    MATCHES_FOUR("4개 일치 (50,000원)", 50_000),
+    MATCHES_THREE("3개 일치 (5,000원)", 5_000),
+    NO_MATCH("꽝 (0원)", 0);
+
+    private final String message;
+    private final long profitMoney;
+
+    WinningType(String message, long profitMoney) {
+        this.message = message;
+        this.profitMoney = profitMoney;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public long getProfitMoney() {
+        return profitMoney;
+    }
+}
